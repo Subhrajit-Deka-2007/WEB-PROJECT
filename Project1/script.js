042efb3e9f192e90ab21272b905acfa9432ba3f3
Subhrajit Deka <dekasubhrajit2007@gmail.com>
2026-02-24 07:53:50 +0530

    Commiting

--- Project1/script.js
+++ Project1/script.js
@@ -1,10 +1,10 @@
 function calculate() {
     let element = document.getElementById("amount");
     let element2 = document.getElementById("percentage");
-    let value = Number(element.value)+((element2.value) / 100) * element.value;
+    let value = Number(element.value) + ((element2.value) / 100) * element.value;
     
-    let p  = document.createElement("p");
-    p.textContent = `Total : ${value}`;
+    let p = document.createElement("p");
+    p.textContent = `Total : ${value} `;
     let div = document.getElementById("tip");
     div.appendChild(p);
     p.id = "total";
